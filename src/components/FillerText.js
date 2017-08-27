33f27553925a2e895b4aafaea39b120b2a7f67a4
wk645 <wk645@nyu.edu>
2017-08-27 17:33:32 -0400

    jsx

---
+++ src/components/FillerText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class FillerText extends React.Component {
+	render() {
+		return (<p>I am a filler text. I can be used to fill your screen. Amazing!</p>)
+	}
+}
+
+export default FillerText
